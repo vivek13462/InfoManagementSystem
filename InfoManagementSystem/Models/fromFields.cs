748bbae63d012c763a894681e648a91c5cef552d
Vivek Tiwari <vivek.13462@gmail.com>
2020-10-31 13:01:58 -0700

    Adding address properties

--- InfoManagementSystem/Models/fromFields.cs
+++ InfoManagementSystem/Models/fromFields.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +10,10 @@ namespace InfoManagementSystem.Models
         public string firstName { get; set; }
         public string lastName { get; set; }
         public DateTime createdOn { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
         public string country { get; set; }
         public string favCar { get; set; }
         public string status { get; set; }
