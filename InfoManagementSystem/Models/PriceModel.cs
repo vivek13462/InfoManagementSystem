b85d8c0c5e78668966e0b6e5e6ebc0e9fdd1d03d
Vivek Tiwari <vivek.13462@gmail.com>
2020-11-03 08:08:36 -0800

    Adding Price Model

---
+++ InfoManagementSystem/Models/PriceModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfoManagementSystem.Models
+{
+    public class PriceModel
+    {
+        public int iPhone { get; set; }
+        public int iWatch { get; set; }
+        public int iPad { get; set; }
+        public int AirPod { get; set; }
+        public PriceModel()
+        {
+            this.iPhone = 799;
+            this.iPhone = 350;
+            this.iPad = 750;
+            this.AirPod = 250;
+        }
+        public void setPrice()
+        {
+            /*Dictionary<string, int> dictPrice = new Dictionary<string, int>();
+            dictPrice.Add("iPhone",799);
+            dictPrice.Add("iWatch", 350);
+            dictPrice.Add("iPad", 750);
+            dictPrice.Add("AirPod", 250);*/
+        }         
+
+        
+        
+    }
+}
