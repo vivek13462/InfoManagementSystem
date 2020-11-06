23b057d8995fc6ee107892c8d880d2907768d596
Vivek Tiwari <vivek.13462@gmail.com>
2020-11-05 18:19:54 -0800

    Adding 2 new Actions for Home Controller

--- InfoManagementSystem/Controllers/HomeController.cs
+++ InfoManagementSystem/Controllers/HomeController.cs
@@ -1,32 +1,65 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OfficeOpenXml;
+using System.IO;
 
 namespace InfoManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        public List<Models.formFieldsModel> excelDataList { get; set; }
         public ActionResult Index()
         {
-            Models.formData fd = new Models.formData();
-            fd.callJotAPI();
-            return View();
+            Models.formDataModel fdObj = new Models.formDataModel();
+            fdObj.callJotFormAPI();
+            return View("Index", fdObj);
         }
 
-        public ActionResult About()
+        public ActionResult CheckDiscount()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Models.existingCustomerModel emObj = new Models.existingCustomerModel();
+            emObj.getCustDetails();
+            return View("DiscountView", emObj);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        public void ExcelFile()
+       {
+           Models.formDataModel fdData = new Models.formDataModel();
+           List<Models.formFieldsModel> LstData;
+           excelDataList = new List<Models.formFieldsModel>();
+           LstData = fdData.callJotFormAPI();
 
-            return View();
-        }
+           for (int i = 0; i < LstData.Count; i++)
+           {
+               Models.formFieldsModel formFields = new Models.formFieldsModel();
+               formFields.custId = Int32.Parse(LstData[i].custId.ToString());
+               formFields.firstName = LstData[i].firstName;
+               formFields.lastName = LstData[i].lastName;
+               formFields.country = LstData[i].country;
+               formFields.email = LstData[i].email;
+               formFields.favProduct = LstData[i].favProduct;
+               excelDataList.Add(formFields);
+           }
+           ExcelPackage excel = new ExcelPackage();
+           var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
+           workSheet.Cells[1, 1].LoadFromCollection(excelDataList, true);
+           using (var rng = workSheet.Cells["A1:AE" + excelDataList.Count + 1])
+           {
+               rng.AutoFitColumns();
+           }
+           using (var memoryStream = new MemoryStream())
+           {
+               Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+               Response.AddHeader("content-disposition", "attachment;  filename=" + "ExcelReport _" + DateTime.Now.Month + DateTime.Now.Day + DateTime.Now.Year + ".xlsx");
+               excel.SaveAs(memoryStream);
+               memoryStream.WriteTo(Response.OutputStream);
+               Response.Flush();
+               Response.End();
+           }
+
+       }
     }
 }
