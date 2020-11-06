5de8849354117ce7ff15c747602226feba95e960
Vivek Tiwari <vivek.13462@gmail.com>
2020-11-05 18:25:02 -0800

    AddingUpdatedformFieldModel.cs

---
+++ InfoManagementSystem/Models/formFieldsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfoManagementSystem.Models
+{
+    public class formFieldsModel
+    {
+        public int custId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime createdOn { get; set; }
+        public string country { get; set; }
+        public string email { get; set; }
+        public string favProduct { get; set; }
+        public double prodPrice { get; set; }
+        public double pastPurchaseAmt { get; set; }
+        public double finalAmt { get; set; }
+        public int perDiscQualified { get; set; }
+        public string status { get; set; }
+    }
+}
